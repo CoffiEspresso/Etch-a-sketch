ec98d3eb0e038fdefa41bf6db40a26c64c038832
Coffi <160792583+CoffiEspresso@users.noreply.github.com>
2024-07-11 20:56:53 +0200

    added: rainbow mode, eraser, grid lines, clear button, adjustable size, color picker

--- javascript.js
+++ javascript.js
@@ -1,47 +1,97 @@
 
 const grid = document.querySelector(".grid");
-const DEFAULT_SIZE = 22;
+const DEFAULT_SIZE = 16;
 const BACKGROUND_GRIDCOLOR = "white";
+
+
+
+const sider = document.querySelector("#slider");
+slider.addEventListener('input', RedrawGrid);
+
+document.querySelector("#gridLines").addEventListener("click", ToggleLines);
 
 const clearButton = document.querySelector("#clearAll");
 clearButton.addEventListener("click", ClearGrid);
-const border = 1;
-let size = grid.clientWidth /DEFAULT_SIZE;
+
+let cells;
+let border = 1;
+let colorType = "pen"; //can get only: pen, eraser, rainbow, or a color
+
+document.querySelector("#eraser").addEventListener("click", (e)=>{colorType = e.target.id});
+document.querySelector("#rainbow").addEventListener("click", (e)=>{colorType = e.target.id});
+document.querySelector("#favcolor").addEventListener("input", (e)=>{colorType = e.target.value});
+
+
+
 //Grid Creation
-for (let i = 0; i < (DEFAULT_SIZE * DEFAULT_SIZE); i++) {
+DrawGrid(DEFAULT_SIZE);
 
-    const div = document.createElement("div");
-    div.classList.add("cell");
-    
-    div.addEventListener("mousedown", Paint);
-    div.addEventListener("mouseenter", Paint);
-    //let size = 100 / DEFAULT_SIZE;
-    
-    div.style.flex = `1 0 ${size}px`;
-    div.style.background = BACKGROUND_GRIDCOLOR;
-    div.style.border = `${border}px solid black`;
-    div.style.maxHeight= size;
+function DrawGrid(dim){    
 
-    grid.appendChild(div);
+    let cellSize = grid.clientWidth /dim;
+    
+    for (let i = 0; i < (dim * dim); i++) {
+        
+        const div = document.createElement("div");
+        div.classList.add("cell");
+        
+        div.addEventListener("mousedown", Paint);
+        div.addEventListener("mouseenter", Paint);
+        
+        div.style.flex = `1 0 ${cellSize}px`;
+        div.style.background = BACKGROUND_GRIDCOLOR;
+        
+        div.classList.add("gridBorder");
+     
+        div.style.maxHeight= cellSize;
+        
+        grid.appendChild(div);
+    }
+     cells = document.querySelectorAll(".cell");
+
 }
-
+    
 
 /* FUNCTIONS */
 
 //What happen when you Paint
 function Paint(e) {
+    let color;
+    switch(colorType){
+        case "pen": color = "black"; break;
+        case "eraser": color = BACKGROUND_GRIDCOLOR; break;
+        case "rainbow": color = "rgb(" + Math.floor(Math.random() * 255)
+                                    + "," + Math.floor(Math.random() * 255) + ","
+                                    + Math.floor(Math.random() * 255) + ")";
+                                    break;
+        default: color = colorType; break;
+    }
+
     e.preventDefault();
     if (e.buttons == 1) {
-        e.target.style.background = "black";
+        e.target.style.background = color ;
         return;
     }
     else;//do nothing?
 }
 
 function ClearGrid() {
-    let cells = document.querySelectorAll(".cell");
     cells.forEach(cell => {
-        cell.style.background = "white";
+        cell.style.background = BACKGROUND_GRIDCOLOR;
     });
     return;
+}
+
+function RedrawGrid(e){  
+    cells.forEach(cell => {
+        cell.remove();
+    });
+
+    DrawGrid(parseInt(slider.value));
+}
+
+function ToggleLines(e){
+    cells.forEach(cell => {
+        cell.classList.toggle("gridBorder");
+    });
 }
